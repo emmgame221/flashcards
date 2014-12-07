0e5531d090de556fd44d48a16d4b27d98aa28b3c
Eric McHugh <emmgame221@gmail.com>
2014-12-07 00:39:14 -0500

    Added menu to form

--- FlashCards/Form1.Designer.cs
+++ FlashCards/Form1.Designer.cs
@@ -28,20 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studyMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.modeMenuItem,
+            this.helpMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveMenuItem,
+            this.toolStripMenuItem1,
+            this.quitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveMenuItem.Text = "Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitMenuItem
+            // 
+            this.quitMenuItem.Name = "quitMenuItem";
+            this.quitMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitMenuItem.Text = "Quit";
+            // 
+            // modeMenuItem
+            // 
+            this.modeMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createMenuItem,
+            this.studyMenuItem});
+            this.modeMenuItem.Name = "modeMenuItem";
+            this.modeMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.modeMenuItem.Text = "Mode";
+            // 
+            // createMenuItem
+            // 
+            this.createMenuItem.Name = "createMenuItem";
+            this.createMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.createMenuItem.Text = "Create";
+            // 
+            // studyMenuItem
+            // 
+            this.studyMenuItem.Name = "studyMenuItem";
+            this.studyMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.studyMenuItem.Text = "Study";
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpMenuItem.Text = "Help";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
             this.Name = "MainForm";
             this.Text = "Flash Cards";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studyMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
     }
 }
 
