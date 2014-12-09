f7b1c22da34c3c9355d04bc225fc375ca93c2503
Eric McHugh <emmgame221@gmail.com>
2014-12-08 20:56:52 -0500

    Added FlashCard and Deck classes for future use

---
+++ FlashCards/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+    class Deck
+    {
+        private List<FlashCard> cards;
+
+        public FlashCard CardAt(int index)
+        {
+            return cards.ElementAt(index);
+        }
+
+        public void Add(FlashCard card)
+        {
+            cards.Add(card);
+        }
+
+        public FlashCard FirstCard()
+        {
+            return cards[0];
+        }
+
+        public Deck() { 
+            cards = new List<FlashCard>();
+        }
+    }
+}
---
+++ FlashCards/FlashCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+    struct FlashCard
+    {
+        private string front;
+        private string back;
+
+        public string Front
+        {
+            get
+            {
+                return front;
+            }
+        }
+
+        public string Back
+        {
+            get
+            {
+                return back;
+            }
+        }
+
+        public FlashCard(string front, string back)
+        {
+            this.front = front;
+            this.back = back;
+        }
+    }
+}
