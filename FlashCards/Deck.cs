00e2f6cef2807b25cd30d3e646f18115c23fa5df
Eric McHugh <emmgame221@gmail.com>
2014-12-08 21:20:04 -0500

    Implemented Shuffle in the Deck class

--- FlashCards/Deck.cs
+++ FlashCards/Deck.cs
@@ -8,6 +8,7 @@ namespace FlashCards
 {
     class Deck
     {
+        Random rand = new Random();
         private List<FlashCard> cards;
 
         public FlashCard CardAt(int index)
@@ -27,6 +28,18 @@ namespace FlashCards
 
         public Deck() { 
             cards = new List<FlashCard>();
+        }
+
+        public void Shuffle()
+        {
+            int n = cards.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int randNum = i + (int)(rand.NextDouble() * (n - 1));
+                FlashCard temp = cards[randNum];
+                cards[randNum] = cards[i];
+                cards[i] = temp;
+            }
         }
     }
 }
